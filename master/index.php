2d249ff4dff15de88b56571ca8727288fd477215
Bernhard Fröhlich <decke@bluelife.at>
2015-03-27 14:04:40 +0100

    Add simple stub for GitHub Webhooks

--- master/index.php
+++ master/index.php
@@ -16,6 +16,22 @@ $app->get('/', function() use ($app) {
    $app->redirect('https://decke.github.io/redports/', 301);
 });
 
+/* GitHub Webhooks */
+$app->post('/github/', function() use ($app) {
+   switch($app->request->headers->get('X-GitHub-Event'))
+   {
+      case 'ping':
+         $app->response->write('pong');
+      break;
+      case 'push':
+         $app->response->write('Push event not implemented yet');
+      break;
+      default:
+         $app->response->write('Event type not implemented');
+      break;
+   }
+});
+
 /* Jobs */
 $app->get('/api/jobs/:jobid', 'isAllowed', function($jobid) use ($app) {
    $app->response->headers->set('Content-Type', 'application/json');
