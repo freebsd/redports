95b8b7f3226c0d6aa4489a2301b018c4e894eec6
Bernhard Fröhlich <decke@bluelife.at>
2015-03-28 12:43:47 +0100

    Implement reading user config from .redports.yml in users git repository.

--- master/lib/Config.php
+++ master/lib/Config.php
@@ -4,7 +4,11 @@ class Config
 {
    protected static $settings = array(
       'datasource' => 'sqlite:/var/db/redports/master.db',
-      'https_only' => true
+      'https_only' => true,
+      'userconfig' => array(
+          'jails' => array('10.0-RELEASE/amd64', '10.0-RELEASE/i386'),
+          'notify' => 'commit' /* commit, email, none */
+      )
    );
 
    public static function get($property)
--- master/lib/GitHubWebhook.php
+++ master/lib/GitHubWebhook.php
@@ -2,6 +2,31 @@
 
 function handleGitHubPushEvent($payload)
 {
-   return true;
+   $config = getUserConfig($payload['repository']['full_name'].'/'.$payload['commits'][0]['id']);
+}
+
+function getUserConfig($commitpath)
+{
+   $defaultconfig = Config::get('userconfig');
+
+   $config = yaml_parse_url("https://raw.githubusercontent.com/".$commitpath."/.redports.yml");
+   if($config === false)
+      return $defaultconfig;
+
+   foreach($config as $key => $value)
+   {
+      if(isset($defaultconfig[$key]))
+      {
+         if(gettype($defaultconfig[$key]) == gettype($value))
+            $defaultconfig[$key] = $value;
+         else
+         {
+            if(settype($value, gettype($defaultconfig[$key])))
+               $defaultconfig[$key] = $value;
+         }
+      }
+   }
+
+   return $defaultconfig;
 }
 
