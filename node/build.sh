d1522e6e627728c66c3a70332e2b1cba45fdde5c
Bernhard Fröhlich <decke@bluelife.at>
2015-04-29 11:29:36 +0200

    Add OpenSSL signature for phar file

--- node/build.sh
+++ node/build.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+# bootstrap box
 if [ ! -f "box.phar" ]; then
    curl -LSs http://box-project.org/installer.php | php
 fi
 
+# create OpenSSL keys for signing
+if [ ! -f "private.key" ]; then
+   php box.phar key:create
+fi
+
+if [ ! -f "public.key" ]; then
+   php box.phar key:extract private.key
+fi
+
+# bootstrap composer
 if [ ! -d "vendor" ]; then
    composer install
 fi
 
+# build phar
 php -d phar.readonly=0 box.phar build
 
