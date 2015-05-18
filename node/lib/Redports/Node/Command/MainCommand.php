e51b299add006ebf894eb8d49c034ea65d92cce0
Bernhard Fröhlich <decke@bluelife.at>
2015-05-18 21:43:26 +0200

    Add some log messages

--- node/lib/Redports/Node/Command/MainCommand.php
+++ node/lib/Redports/Node/Command/MainCommand.php
@@ -2,6 +2,7 @@
 
 namespace Redports\Node\Command;
 
+use Redports\Node\Config;
 use Redports\Node\ProcessManager;
 use Redports\Node\Poudriere\Poudriere;
 
@@ -9,11 +10,20 @@ class MainCommand extends Command
 {
    function execute($options, $arguments)
    {
+      $logger = Config::getLogger();
       $pm = new ProcessManager();
       $poudriere = new Poudriere();
 
       foreach($poudriere->getAllJails() as $jail)
-         $pm->addJail($jail->getJailname());
+      {
+         if($jail->getQueue() === null)
+            $logger->warning('Ignoring Jail '.$jail->getJailname().' because Queue is not defined');
+         else
+         {
+            $logger->info('Adding Jail '.$jail->getJailname().' to Queue '.$jail->getQueue());
+            $pm->addJail($jail->getJailname());
+         }
+      }
 
       $pm->run();
    }
