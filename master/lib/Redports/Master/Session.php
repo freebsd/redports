98ad6221ed076726a470b6fef6e0c2819ce1607a
Bernhard Fröhlich <decke@bluelife.at>
2016-01-22 12:05:51 +0100

    Cleanup comments.

--- master/lib/Redports/Master/Session.php
+++ master/lib/Redports/Master/Session.php
@@ -28,34 +28,35 @@ class Session
 
     public static function initialize()
     {
-      // Set Redis as session handler
+      /* Set Redis as session handler */
       ini_set('session.save_handler', 'redis');
       ini_set('session.save_path', 'unix:///var/run/redis/redis.sock?persistent=1');
 
-      // Specify hash function used for session ids. Usually does not
-      // work on FreeBSD unless hash functions are compiled into the binary
-      //ini_set('session.hash_function', 'sha256');
+      /* Specify hash function used for session ids. Usually does not
+       * work on FreeBSD unless hash functions are compiled into the binary
+       */
+      /* ini_set('session.hash_function', 'sha256'); */
       ini_set('session.hash_bits_per_character', 5);
       ini_set('session.entropy_length', 512);
 
-      // Set session lifetime in redis (8h)
+      /* Set session lifetime in redis (8h) */
       ini_set('session.gc_maxlifetime', 28800);
 
-      // Set cookie lifetime on client
+      /* Set cookie lifetime on client */
       ini_set('session.cookie_lifetime', 0);
 
-      // do not expose Cookie value to JavaScript (enforced by browser)
+      /* do not expose Cookie value to JavaScript (enforced by browser) */
       ini_set('session.cookie_httponly', 1);
 
       if (Config::get('https_only') === true) {
-          // only send cookie over https
+          /* only send cookie over https */
           ini_set('session.cookie_secure', 1);
       }
 
-      // prevent caching by sending no-cache header
+      /* prevent caching by sending no-cache header */
       session_cache_limiter('nocache');
 
-      // rename session
+      /* rename session */
       session_name('SESSIONID');
     }
 
